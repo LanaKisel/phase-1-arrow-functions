24424342664307ff7a9cdfad30b4d83924edf345
Lana Kisel <svetlana.kisel.ca@gmail.com>
2023-07-03 12:14:57 -0700

    arrow functions

---
+++ index.js
@@ -0,0 +1,8 @@
+function divide(num1,num2){
+    return num1/num2
+}
+divide(2000, 100)
+
+const square = parameter => parameter*parameter;
+
+const add = (parameter1, parameter2) => parameter1+parameter2;
